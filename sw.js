5de844316b057398604188338b7b89fefc24cf12
abauer726 <abauer726@gmail.com>
2023-08-03 10:22:55 -0700

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-c078a73049dfa263ce0e.js"
+    "url": "webpack-runtime-13ebf2107b2b45f3770a.js"
   },
   {
     "url": "framework-025a10830d88a05b2804.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "68510e3c1a3fe1062843b30ba426eeb7"
+    "revision": "cbb5b18e3494bcccb76ade76f9b43f51"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-9b1997f2483a747a5203.js"
