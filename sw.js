a557304ce66bc90bf79ecca563864f04529487d7
Anna Bauer <abauer726@gmail.com>
2025-05-07 14:10:37 -0700

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-acc175355390405cfbc7.js"
+    "url": "webpack-runtime-e019fb286bc23e2fca78.js"
   },
   {
     "url": "framework-025a10830d88a05b2804.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "203d7bd13aa43e18c41469299b152042"
+    "revision": "fe4cdbc6e2b4dd620a67a5600a8bda34"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-9b1997f2483a747a5203.js"
